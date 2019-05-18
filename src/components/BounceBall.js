32fa978653049835cedde051a6e453257fcbb31f
Nick Ray <nray@nray.io>
2019-05-18 11:21:06 -0600

    Add task cost

--- src/components/BounceBall.js
+++ src/components/BounceBall.js
@@ -1,7 +1,9 @@
 import React, { useLayoutEffect, useState, useRef } from "react";
 import styled from "styled-components";
+import Slider from "react-rangeslider";
+import "react-rangeslider/lib/index.css";
 
-const BALL_WIDTH = 80;
+const BALL_WIDTH = 80; // in px
 
 const Ball = styled.div`
   background: ${props => props.color};
@@ -15,6 +17,17 @@ const Container = styled.div`
   height: 100%;
   padding-top: 25px;
   padding-bottom: 25px;
+`;
+
+const SliderContainer = styled.div`
+  padding-top: 50px;
+  .rangeslider-horizontal .rangeslider__fill {
+    background-color: #c57070;
+  }
+`;
+
+const Caption = styled.div`
+  font-size: 1.5rem;
 `;
 
 function useAnimationFrame(callback) {
@@ -41,26 +54,50 @@ function useAnimationFrame(callback) {
   }, []);
 }
 
+function runJank(duration) {
+  const startTime = performance.now();
+
+  // simulate a running task
+  while (performance.now() - startTime < duration) {}
+}
+
 export default function({ color }) {
   const [x, setX] = useState(0);
+  const [frameBudget, setFrameBudget] = useState(0);
+  const [jank, setJank] = useState(0);
   const containerEl = useRef();
   const containerWidth = useRef();
   const startTime = useRef(performance.now());
   const velocity = useRef(500); // 500 pixels / second
+  const lastUpdatedFrameBudget = useRef(startTime.current);
 
   useLayoutEffect(() => {
     containerWidth.current = containerEl.current.clientWidth;
-  });
+  }, []);
 
   useAnimationFrame(() => {
+    runJank(jank);
+
     const now = performance.now();
 
+    requestIdleCallback(
+      deadline => {
+        if (now - lastUpdatedFrameBudget.current >= 200) {
+          lastUpdatedFrameBudget.current = now;
+          setFrameBudget(deadline.timeRemaining().toFixed(0));
+        }
+      },
+      { timeout: 16 }
+    );
+
     if (x + BALL_WIDTH >= containerWidth.current && velocity.current > 0) {
+      // reset the clock and change velocity direction
       startTime.current = now;
       velocity.current = -velocity.current;
     }
 
     if (x <= 0 && velocity.current < 0) {
+      // reset the clock and change velocity direction
       startTime.current = now;
       velocity.current = Math.abs(velocity.current);
     }
@@ -82,6 +119,23 @@ export default function({ color }) {
   return (
     <Container ref={containerEl}>
       <Ball color={color} style={{ transform: `translateX(${x}px)` }} />
+      <div>
+        <strong>Frame Budget</strong>:{" "}
+        {frameBudget > 0 ? `${frameBudget} ms` : "😭"}
+      </div>
+      <SliderContainer>
+        <Slider
+          min={0}
+          max={100}
+          value={jank}
+          onChange={value => {
+            setJank(value);
+          }}
+        />
+        <Caption>
+          <strong>Task Cost</strong>: {jank} ms
+        </Caption>
+      </SliderContainer>
     </Container>
   );
 }
